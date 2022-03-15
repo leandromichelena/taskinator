e7e33c6a0f5d713ae30e61923dce2459940b5e51
Leandro Michelena <leandromichelena@gmail.com>
2022-03-15 00:48:10 -0400

    Added function to the submit form input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function () {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
+var createTaskHandler = function (event) {
+    // console.log(event);
+    event.preventDefault(); // prevents the browser from refreshing after submitting the form
+    var listItemEl = document.createElement("li"); // creates the <li> html element
+    listItemEl.className = "task-item"; // assigns the class task-item to the <li> element
     listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+    tasksToDoEl.appendChild(listItemEl); // adds the li element to the #tasks-to-do <ul> 
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
+// submit event is also called onsubmit in certain documentation
+
